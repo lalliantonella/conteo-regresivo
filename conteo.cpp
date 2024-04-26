258af04ef7a79d905e271923fcaeb7ca28d86459
lalliantonella <lalliantonella03@gmail.com>
2024-04-26 12:07:03 -0300

    first commit

---
+++ conteo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int cuenta;
+ for (int cuenta = 100 ; cuenta  >= 1; --cuenta) {
+            cout << cuenta << endl;
+};
+
+                   return 0;
+}
